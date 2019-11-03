ac2534a2f962ba88f9c67693a2ff5fe26f9bd484
Gary Tyler <mail@garytyler.com>
2019-11-03 18:41:15 -0500

    Use familiar frozen destination file structure for vlc binary dependencies, on all platforms

--- fbs/freeze/hooks/hook-vlc.py
+++ fbs/freeze/hooks/hook-vlc.py
@@ -1,44 +1,48 @@
 import os
-from os.path import join, dirname, relpath, commonpath
+from os.path import join, dirname, relpath, commonpath, abspath
 from glob import glob
 from importlib import import_module
-
 from PyInstaller.compat import is_darwin, is_win, is_linux
 
 
 if is_linux:
-    DYLIB_PATTERN='*.dylib'
+    DYLIB_PATTERN = "lib*.so"
 elif is_win:
-    DYLIB_PATTERN='*.dll'
+    DYLIB_PATTERN = "*.dll"
 elif is_darwin:
-    DYLIB_PATTERN='lib*.so'
+    DYLIB_PATTERN = "*.dylib"
 
 
 def hook(hook_api):
     if not hook_api.__name__ == "vlc":
         return None
 
-    libvlc_src_file = os.environ["PYTHON_VLC_LIB_PATH"]
-    plugin_src_dir = os.environ["PYTHON_VLC_MODULE_PATH"]
+    dst_root_name = "vlc"
 
-    # Get common root
-    common_root = commonpath([libvlc_src_file, plugin_src_dir])
+    libvlc_src_file = abspath(os.environ["PYTHON_VLC_LIB_PATH"])
+    plugin_src_dir = abspath(os.environ["PYTHON_VLC_MODULE_PATH"])
 
     # Add libvlc binaries
     libvlc_src_files = glob(join(dirname(libvlc_src_file), DYLIB_PATTERN))
     libvlc_binaries = []
     for f in libvlc_src_files:
-        binary_tuple = (f, ".")
+        binary_tuple = (f, dst_root_name)
         libvlc_binaries.append(binary_tuple)
     hook_api.add_binaries(libvlc_binaries)
 
     # Add plugin binaries
     plugin_src_files = []
     for root, _, __ in os.walk(plugin_src_dir):
-        plugin_src_files.extend(glob(join(root, DYLIB_PATTERN)))
+        pattern_path = join(root, DYLIB_PATTERN)
+        plugin_src_files.extend(glob(pattern_path))
+
+    # print(plugin_src_files)
     plugin_binaries = []
     for f in plugin_src_files:
-        rel_dir = relpath(dirname(f), common_root)
-        bin_tuple = (f, rel_dir)
+        src_plugins_root = f
+        while not src_plugins_root.endswith("plugins"):
+            src_plugins_root = dirname(src_plugins_root)
+        dst_rel_dir = join(dst_root_name, "plugins", relpath(f, src_plugins_root))
+        bin_tuple = (f, dst_rel_dir)
         plugin_binaries.append(bin_tuple)
     hook_api.add_binaries(plugin_binaries)
