aab229fd90a431de4173105475591e8d875dd02e
Gary Tyler <mail@garytyler.com>
2019-11-03 21:48:03 -0500

    Use frozen app binaries root as vlc binary destination root

--- fbs/freeze/hooks/hook-vlc.py
+++ fbs/freeze/hooks/hook-vlc.py
@@ -17,8 +17,6 @@ def hook(hook_api):
     if not hook_api.__name__ == "vlc":
         return None
 
-    dst_root_name = "vlc"
-
     libvlc_src_file = abspath(os.environ["PYTHON_VLC_LIB_PATH"])
     plugin_src_dir = abspath(os.environ["PYTHON_VLC_MODULE_PATH"])
 
@@ -26,15 +24,15 @@ def hook(hook_api):
     libvlc_src_files = glob(join(dirname(libvlc_src_file), DYLIB_PATTERN))
     libvlc_binaries = []
     for f in libvlc_src_files:
-        binary_tuple = (f, dst_root_name)
+        binary_tuple = (f, ".")
         libvlc_binaries.append(binary_tuple)
     hook_api.add_binaries(libvlc_binaries)
 
     # Add plugin binaries
     plugin_src_files = []
     for root, _, __ in os.walk(plugin_src_dir):
-        pattern_path = join(root, DYLIB_PATTERN)
-        plugin_src_files.extend(glob(pattern_path))
+        pattern = join(root, DYLIB_PATTERN)
+        plugin_src_files.extend(glob(pattern))
 
     # print(plugin_src_files)
     plugin_binaries = []
@@ -42,7 +40,7 @@ def hook(hook_api):
         src_plugins_root = f
         while not src_plugins_root.endswith("plugins"):
             src_plugins_root = dirname(src_plugins_root)
-        dst_rel_dir = join(dst_root_name, "plugins", relpath(f, src_plugins_root))
-        bin_tuple = (f, dst_rel_dir)
-        plugin_binaries.append(bin_tuple)
+        dst_rel_dir = join("plugins", relpath(f, src_plugins_root))
+        binary_tuple = (f, dst_rel_dir)
+        plugin_binaries.append(binary_tuple)
     hook_api.add_binaries(plugin_binaries)
